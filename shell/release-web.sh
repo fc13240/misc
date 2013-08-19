cfbc5c499be860d9bd34e298f528da2bf556c3c8
tonny <wodexintiao@gmail.com>
2013-08-19 11:37:24 +0800

    change order

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -8,45 +8,45 @@ CURRENT_DIR=`dirname $0`
 WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
 # change work directory first
-cd $WORK_DIR;
 echo "<b>1. cd $WORK_DIR;</b>";
+cd $WORK_DIR;
 echo $NEWLINE;
 
 # remove modify
+echo "<b>2. git stash;</b>";
 echo '<pre>';
 git add .
 git stash
 git stash drop
 echo '</pre>';
-echo "<b>2. git stash;</b>";
 echo $NEWLINE;
 
 # pull source
+echo "<b>3. git pull origin master;</b>";
 echo '<pre>';
 git pull origin master
 echo '</pre>';
-echo "<b>3. git pull origin master;</b>";
 echo $NEWLINE;
 
 # tag
+echo "<b>4. git tag $CURRENT_TIME;</b>";
 echo '<pre>';
 git tag $CURRENT_TIME;
 echo '</pre>';
-echo "<b>4. git tag $CURRENT_TIME;</b>";
 echo $NEWLINE;
 
 # compress source/* -> min/*
+echo "<b>5. compress</b>";
 echo '<pre>';
 /usr/local/bin/node /home/sam/nodejs/compresser/index.js $WORK_DIR/source $WORK_DIR/min
 echo '</pre>';
-echo "<b>5. compress finished;</b>";
 echo $NEWLINE;
 
 # rsync
+echo "<b>6. rsync</b>";
 echo '<pre>';
 # rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.220:/home/sam/www/htdocs/n/
 echo '</pre>';
-echo "<b>6. rsync finished;</b>";
 echo $NEWLINE;
 
 echo "<b>7. release finished</b>";
