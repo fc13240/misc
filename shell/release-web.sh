101dbc80140c685d0b6cd3ea462a712a18ffbd71
tonny <wodexintiao@gmail.com>
2013-08-19 09:28:12 +0800

    exec user change to sam

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -2,9 +2,9 @@
 
 CURRENT_DIR=`dirname $0`
 BASE_DIR=`cd $CURRENT_DIR;cd ..;pwd`
-su - sam << EOF
-	cd $BASE_DIR;
-	echo 'cd $BASE_DIR;git pull origin master';
-	git pull origin master
-EOF
+
+cd $BASE_DIR;
+echo 'cd $BASE_DIR;git pull origin master';
+git pull origin master
+
 echo 'release down'
