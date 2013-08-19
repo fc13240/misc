852f517b494ad3c44c90dceb8f81b626488c68ca
tonny <wodexintiao@gmail.com>
2013-08-19 09:31:53 +0800

    a

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -4,7 +4,7 @@ CURRENT_DIR=`dirname $0`
 BASE_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
 cd $BASE_DIR;
-echo 'cd $BASE_DIR;git pull origin master';
+echo "cd $BASE_DIR;git pull origin master";
 git pull origin master
 
 echo 'release down'
