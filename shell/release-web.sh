f8693f703bda67e38547ed269656944741ba35dc
tonny <wodexintiao@gmail.com>
2013-08-19 11:32:23 +0800

    a

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -9,28 +9,34 @@ WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
 # change work directory first
 cd $WORK_DIR;
-echo "1. cd $WORK_DIR;$NEWLINE";
+echo "<b>1. cd $WORK_DIR;</b>";
+echo $NEWLINE;
 
 # remove modify
 git add .
 git stash
 git stash drop
-echo "2. git stash;$NEWLINE";
+echo "<b>2. git stash;</b>";
+echo $NEWLINE;
 
 # pull source
 git pull origin master
-echo "3. git pull origin master;$NEWLINE";
+echo "<b>3. git pull origin master;</b>";
+echo $NEWLINE;
 
 # tag
 git tag $CURRENT_TIME;
-echo "4. git tag $CURRENT_TIME;$NEWLINE";
+echo "<b>4. git tag $CURRENT_TIME;</b>";
+echo $NEWLINE;
 
 # compress source/* -> min/*
 /usr/local/bin/node /home/sam/nodejs/compresser/index.js $WORK_DIR/source $WORK_DIR/min
-echo "5. compress finished;$NEWLINE";
+echo "<b>5. compress finished;</b>";
+echo $NEWLINE;
 
 # rsync
 # rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.220:/home/sam/www/htdocs/n/
-echo "6. rsync finished;$NEWLINE";
+echo "<b>6. rsync finished;</b>";
+echo $NEWLINE;
 
-echo "7. release finished";
+echo "<b>7. release finished</b>";
