e226ef87b4a05151b3fac0ddb288b44174793999
tonny <wodexintiao@gmail.com>
2013-08-19 11:35:14 +0800

    add pre

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -13,29 +13,39 @@ echo "<b>1. cd $WORK_DIR;</b>";
 echo $NEWLINE;
 
 # remove modify
+echo '<pre>';
 git add .
 git stash
 git stash drop
+echo '</pre>';
 echo "<b>2. git stash;</b>";
 echo $NEWLINE;
 
 # pull source
+echo '<pre>';
 git pull origin master
+echo '</pre>';
 echo "<b>3. git pull origin master;</b>";
 echo $NEWLINE;
 
 # tag
+echo '<pre>';
 git tag $CURRENT_TIME;
+echo '</pre>';
 echo "<b>4. git tag $CURRENT_TIME;</b>";
 echo $NEWLINE;
 
 # compress source/* -> min/*
+echo '<pre>';
 /usr/local/bin/node /home/sam/nodejs/compresser/index.js $WORK_DIR/source $WORK_DIR/min
+echo '</pre>';
 echo "<b>5. compress finished;</b>";
 echo $NEWLINE;
 
 # rsync
+echo '<pre>';
 # rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.220:/home/sam/www/htdocs/n/
+echo '</pre>';
 echo "<b>6. rsync finished;</b>";
 echo $NEWLINE;
 
