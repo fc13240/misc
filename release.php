d60cf79de5a2c9878963948254f706bb45ae6485
tonny <wodexintiao@gmail.com>
2013-08-19 14:37:08 +0800

    add filter repeat post

--- release.php
+++ release.php
@@ -1,3 +1,6 @@
+<?php 
+session_start();
+?>
 <!doctype html>
 <html>
 <head>
@@ -5,22 +8,36 @@
 </head>
 <body style="width:700px;margin: 10px auto">
 <?php
+$isShowForm = true;
 if(!empty($_POST)){
-	$type = $_POST['release-type'];
-	$shellName = ($type == 1?'release-misc.sh':'release-web.sh');
-	$command = '/bin/bash '.$_SERVER['DOCUMENT_ROOT'].'/shell/'.$shellName;
-	echo $command.'<br/>';
-	passthru ($command,$res);
-	var_dump($res);
-	echo '<br/><a href="">发布成功</a>';
-}else{?>
+	if($_SESSION['flag'] != $_POST['flag']){
+		echo '<script>alert("请不要重复发布！");location.href=""</script>';
+	}else{
+		$isShowForm = false;
+		$type = $_POST['release-type'];
+		$shellName = ($type == 1?'release-misc.sh':'release-web.sh');
+		$command = '/bin/bash '.$_SERVER['DOCUMENT_ROOT'].'/shell/'.$shellName;
+		echo $command.'<br/>';
+		passthru ($command,$res);
+		echo '<br/>';
+		var_dump($res);
+		echo '<br/><a href="">发布成功,点击刷新</a>';
+		$_SESSION['flag'] = time();
+	}
+}
+if($isShowForm){
+?>
 	<form method="POST">
-		<input type="hide" name="release-type" value="1"/>
-		<input type="submit" value="release-misc" style="width:150px;"/>
+		<input type="hidden" name="flag" value="<?php echo $_SESSION['flag'];?>"/>
+		<input type="hidden" name="release-type" value="1"/>
+		<input type="submit" value="发布前端网站" style="width:150px;float:left;margin-right:10px;"/>
+		<p>发布前端网站资源</p>
 	</form>
 	<form method="POST">
-		<input type="hide" name="release-type" value="2"/>
-		<input type="submit" value="release-web" style="width:150px;"/>
+		<input type="hidden" name="flag" value="<?php echo $_SESSION['flag'];?>"/>
+		<input type="hidden" name="release-type" value="2"/>
+		<input type="submit" value="发布前端资源" style="width:150px;float:left;margin-right:10px;"/>
+		<p>会把source下文件同步到生产服务器</p>
 	</form>
 <?php }?>
 </body>
--- shell/release-web.sh
+++ shell/release-web.sh
@@ -1,61 +1,14 @@
 #!/bin/bash
 
-NEWLINE='<br/>';
-CURRENT_TIME=$(date +%Y%m%d_%H%M%S);
-
 # must be absolute path
 CURRENT_DIR=`dirname $0`
 WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
-# change work directory first
-echo "<b>1. cd $WORK_DIR;</b>";
-echo '<pre>';
-cd $WORK_DIR;
-echo '</pre>';
-echo $NEWLINE;
-
-# remove modify
-echo "<b>2. git stash;</b>";
-echo '<pre>';
-git add .
-git stash
-git stash drop
-echo '</pre>';
-echo $NEWLINE;
-
-# pull source
-echo "<b>3. git pull origin master;</b>";
-echo '<pre>';
-git pull origin master
-echo '</pre>';
-echo $NEWLINE;
-
-# tag
-echo "<b>4. git tag $CURRENT_TIME;</b>";
-echo '<pre>';
-git tag $CURRENT_TIME;
-echo '</pre>';
-echo $NEWLINE;
-
-# replace @VERSION
-echo "<b>5. replace @VERSION -> $CURRENT_TIME</b>";
-echo '<pre>';
-sed -i "s/@VERSION/$CURRENT_TIME/" $WORK_DIR/source/j/version.js
-echo '</pre>';
-echo $NEWLINE;
-
-# compress source/* -> min/*
-echo "<b>6. compress</b>";
-echo '<pre>';
-/usr/local/bin/node /home/sam/nodejs/compresser/index.js $WORK_DIR/source $WORK_DIR/min --colors=false
-echo '</pre>';
-echo $NEWLINE;
-
 # rsync
-echo "<b>7. rsync</b>";
+echo "<b>rsync</b>";
 echo '<pre>';
+# rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.220:/home/sam/www/htdocs/n/
+# echo '==============';
 # rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.220:/home/sam/www/htdocs/n/
 echo '</pre>';
-echo $NEWLINE;
-
-echo "<b>8. release finished</b>";
+echo '<br/>';
