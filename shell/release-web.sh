f8963f2e2dca9feaa36298b7a4dd181f20860f30
tonny <wodexintiao@gmail.com>
2013-10-11 16:57:23 +0800

    fixed release

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -7,8 +7,8 @@ WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 # rsync
 echo "<b>rsync</b>";
 echo '<pre>';
-# rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.220:/home/sam/www/htdocs/n/
+rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.223:/home/sam/www/misc/min/
 # echo '==============';
-# rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.220:/home/sam/www/htdocs/n/
+rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.223:/home/sam/www/misc/source/
 echo '</pre>';
 echo '<br/>';
