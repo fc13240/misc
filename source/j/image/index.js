b4c8f14904338415dc7bdb96969050ad090e84b9
yuhan <yuh@weather.com.cn>
2013-10-30 11:22:50 +0800

    imageindexcss

--- source/j/image/index.js
+++ source/j/image/index.js
@@ -183,7 +183,8 @@ $(document).ready(function() {
 			bimg.src = bigurl;
 			$(".download > a").attr("href", "http://search.weather.com.cn/static/download.php?filename=" + bigurl);
 			this.description.html(desc);
-			this.currentnum.html(this.current < 9 ? "0" + (this.current + 1) : this.current + 1);
+			//this.currentnum.html(this.current < 9 ? "0" + (this.current + 1) : this.current + 1);
+			this.currentnum.html(this.current);
 		};
 		this.next = function() {
 			if (!that.animating && this.page <= this.pages) {
