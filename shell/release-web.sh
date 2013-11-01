82f9732dc4e35c4dc6b1dff04c2545ab564c0a1a
tonny <wodexintiao@gmail.com>
2013-11-01 10:30:57 +0800

    add 61.4.185.212 to rsync

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -5,23 +5,23 @@ CURRENT_DIR=`dirname $0`
 WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
 # rsync
-echo "<b>rsync min to 61.4.185.223</b>";
+echo "<b>1. rsync min to 61.4.185.223</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.223:/home/sam/www/misc/min/
 echo '==============';
 echo '</pre>';
-echo "<b>rsync min to 61.4.185.212</b>";
+echo "<b>2. rsync min to 61.4.185.212</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.212:/home/sam/www/misc/min/
 echo '==============';
 echo '</pre>';
 
-echo "<b>rsync source to 61.4.185.223</b>";
+echo "<b>3. rsync source to 61.4.185.223</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.223:/home/sam/www/misc/source/
 echo '==============';
 echo '</pre>';
-echo "<b>rsync source to 61.4.185.212</b>";
+echo "<b>4. rsync source to 61.4.185.212</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.212:/home/sam/www/misc/source/
 echo '==============';
