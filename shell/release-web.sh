30d9664df8f390ca0555102cc8aceebe20e8766c
tonny <wodexintiao@gmail.com>
2013-11-01 10:27:58 +0800

    add 61.4.185.212 to rsync

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -5,10 +5,25 @@ CURRENT_DIR=`dirname $0`
 WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
 # rsync
-echo "<b>rsync</b>";
+echo "<b>rsync min to 61.4.185.223</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.223:/home/sam/www/misc/min/
 echo '==============';
+echo '<pre>';
+echo "<b>rsync min to 61.4.185.212</b>";
+echo '<pre>';
+rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.212:/home/sam/www/misc/min/
+echo '==============';
+echo '</pre>';
+
+echo "<b>rsync source to 61.4.185.223</b>";
+echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.223:/home/sam/www/misc/source/
+echo '==============';
+echo '</pre>';
+echo "<b>rsync source to 61.4.185.212</b>";
+echo '<pre>';
+rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.212:/home/sam/www/misc/source/
+echo '==============';
 echo '</pre>';
 echo '<br/>';
