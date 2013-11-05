89e36f4f29dbda8318040d28c69a8273a287912f
tonny <wodexintiao@gmail.com>
2013-11-05 16:02:01 +0800

    add resize event

--- source/j/ad/caoyu-min.js
+++ source/j/ad/caoyu-min.js
@@ -34,6 +34,6 @@ Namespace=new Object();Namespace.register=function(fullNS){var nsArray=fullNS.sp
 	W(function(){
 		//窗口更改大小事件
 		$(window).on('resize',rePosAd);
-	})
+	});
 	(W.util || (W.util = {}))['adPos'] = rePosAd
 }()
