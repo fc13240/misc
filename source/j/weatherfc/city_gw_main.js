82af633b1850ba85f400b35ba41f2895d0791c0d
yuhan <yuh@weather.com.cn>
2013-11-26 16:59:15 +0800

    city_gw

--- source/j/weatherfc/city_gw_main.js
+++ source/j/weatherfc/city_gw_main.js
@@ -12,43 +12,7 @@ define(function(require){
 	//require('./life_golf');
 	//require('./life_shr');
 	require('./hotcity');
-	var arrowStyle = {fill: "#ee842f", stroke: "#ee842f", "stroke-width": 1};
-		paper = Raphael("flHolder", 84, 84),
-		x = 46,
-		y = 42,
-		r = 20,
-		ang = 0,
-		arrowR = 10;
-	if(flAngle <= '22.5' || (flAngle > '337.6' && flAngle <= '360')){
-		y = y - r;
-		ang = 180;
-	}else if(flAngle > '22.6' && flAngle <= '67.5'){
-		x = x+r*Math.cos(45);
-		y = y - r*Math.sin(45);
-		ang = 225;
-	}else if(flAngle > '67.6' && flAngle <= '112.5'){
-		x = x+r;
-		ang = 270;
-	}else if(flAngle > '112.6' && flAngle <= '157.5'){
-		x = x+r*Math.cos(45);
-		y = y + r*Math.sin(45);
-		ang = 315;
-	}else if(flAngle > '157.6' && flAngle <= '202.5'){
-		y = y + r;
-		ang = 360;
-	}else if(flAngle > '202.6' && flAngle <= '247.5'){
-		x = x-r*Math.cos(45);
-		y = y + r*Math.sin(45);
-		ang = 45;
-	}else if(flAngle > '247.6' && flAngle <= '292.5'){
-		x = x - r;
-		ang = 90;
-	}else if(flAngle > '292.6' && flAngle <= '337.5'){
-		x = x-r*Math.cos(45);
-		y = y - r*Math.sin(45);
-		ang = 135;
-	}
-	paper.polygon(x,y,arrowR).attr(arrowStyle).animate({transform:["t0,0r"+ang]},500);
+	
 	//顶部导航
 	$("nav ul li").click(function(){
 		var num = $(this).find(".dropList a").length,
