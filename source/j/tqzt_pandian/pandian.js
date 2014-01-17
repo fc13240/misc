df4c72aaa943e6e02478294bdab0aca486c14e22
liujinyu <13581642285@163.com>
2014-01-17 11:20:11 +0800

    ljy change name product to products and change src

--- source/j/tqzt_pandian/pandian.js
+++ source/j/tqzt_pandian/pandian.js
@@ -1,13 +1,13 @@
 $(function(){
 	$('.blog>h1>span:first').hover(function(){
-		$(this).children('img').attr("src","http://localhost/source/i/tqzt_pandian/sina2.jpg");
-		$(this).next().children("img").attr("src","http://localhost/source/i/tqzt_pandian/tq1.jpg");
+		$(this).children('img').attr("src","http://i.tq121.com.cn/i/tqzt_pandian/sina2.jpg");
+		$(this).next().children("img").attr("src","http://i.tq121.com.cn/i/tqzt_pandian/tq1.jpg");
 		$(".sinablog").show();
 		$(".tqblog").hide();
 	});
 	$('.blog>h1>span:last').hover(function(){
-		$(this).children('img').attr("src","http://localhost/source/i/tqzt_pandian/tq2.jpg");
-		$(this).prev().children("img").attr("src","http://localhost/source/i/tqzt_pandian/sina1.jpg");
+		$(this).prev().children("img").attr("src","http://i.tq121.com.cn/i/tqzt_pandian/sina1.jpg");
+		$(this).children('img').attr("src","http://i.tq121.com.cn/i/tqzt_pandian/tq2.jpg");
 		$(".sinablog").hide();
 		$(".tqblog").show();
 	});
