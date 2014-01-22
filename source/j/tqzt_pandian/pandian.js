9000acb0abae90bbe4dcda868e627dfd0f40b466
liujinyu <13581642285@163.com>
2014-01-22 09:32:44 +0800

    ljy change

--- source/j/tqzt_pandian/pandian.js
+++ source/j/tqzt_pandian/pandian.js
@@ -14,14 +14,14 @@ $(function(){
 	
 	$(".pdShow .img1,.pdShow .img2,.pdShow .img5,.pdShow .img7,.pdShow .img9").hover(function(){
 		var w=$(this).width();
-		$(this).children("div").stop(true,true).animate({left:'-'+w},400);
+		$(this).children("a").stop(true,true).animate({left:'-'+w},400);
 	},function(){
-		$(this).children("div").stop(true,true).animate({left:0},400);
+		$(this).children("a").stop(true,true).animate({left:0},400);
 	})
 	$(".pdShow .img3,.pdShow .img6,.pdShow .img8,.pdShow .img4").hover(function(){
 		var h=$(this).height();
-		$(this).children("div").stop(true,true).animate({top:'-'+h},400);
+		$(this).children("a").stop(true,true).animate({top:'-'+h},400);
 	},function(){
-		$(this).children("div").stop(true,true).animate({top:0},400);
+		$(this).children("a").stop(true,true).animate({top:0},400);
 	})
 })
