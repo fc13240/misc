d3ca61d8ac5307c4c6501dde801cde4423b7dc80
liujinyu <13581642285@163.com>
2014-02-27 10:44:16 +0800

    ljy

---
+++ source/j/products/chungeng.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(function(){
+	$('area').hover(function(){
+		var name = $(this).attr('data-id');
+		for(var i=0;i<$('.con>div').length;i++){
+			if($('.con>div').eq(i).attr("data-id")==name){
+				$('.con>div').eq(i).show();
+			}
+		}
+	},function(){
+		$(".con>div").hide();
+	})                                                              
+})
