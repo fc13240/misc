77a6a6c1aed7eaa7ed99ae5d1ee4bd762735cede
liqiang <liqiang@weather.com.cn>
2014-04-01 09:09:13 +0800

    index

--- source/j/english/cityInfo.js
+++ source/j/english/cityInfo.js
@@ -121,7 +121,7 @@ define(function(require){
 	$()
 	
     var ide=$(".con .left .act li.yb:first .wea").text();
-	$("p.xianx").html(ide);
+	$(".con .left .act li.sk p.wea").html(ide);
 
 	//七天预报
 	$(".day7 h1 span").click(function(){
