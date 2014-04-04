818f0df824e9d9c6a5628d0d0a92ae913b18e5ba
liujinyu <13581642285@163.com>
2014-04-04 19:26:15 +0800

    ljy

--- source/j/english/home.js
+++ source/j/english/home.js
@@ -5,6 +5,143 @@ define(function(require){
 	require('../tool/tool_pngfix');	
 
 	$(function(){
+
+		//温度格式切换
+		$("#weaUnit li:first").click(function(){
+			$(this).siblings().removeClass('on');
+			$(this).addClass('on');
+			weaTypeFunS();
+		}).click()
+		$("#weaUnit li:last").click(function(){
+			$(this).siblings().removeClass('on');
+			$(this).addClass('on');
+			weaTypeFunF();
+		})
+		
+		
+		//浏览历史 最近3个城市 Local Weather 模块 cookie
+		 function weaTypeFunS(){
+		 	//local weather
+		 	var defaultCityHistory = '101010100,101020100,101280101';  //默认城市 北上广
+		 	cityHistory = $.cookie('cityHistory') || defaultCityHistory;  //get cookie
+
+		 	var chNum = cityHistory.split(',');
+		 	var cityHis_index = 0;	
+		 	$(".localWeather dt").html("°C")
+		 	function setWeather(){  //递归法 解决for循环 异步执行 数据覆盖问题
+		 		$.ajax({
+		 			type:'GET',
+		 			url:"http://d1.weather.com.cn/fc_24_en/"+chNum[cityHis_index]+".html",
+		 			dataType:'script',
+		 			cache:false,
+		 			async:false,
+		 			success:function(type){
+		 				var $li = $(".localWeather ul li").eq(cityHis_index);
+		 				//alert(fc_24_en.weatherinfo.city);
+		 				// $li.click(function(){
+		 				//  	window.open('http://en.weather.com.cn/weather/'+fc_24_en.weatherinfo.cityid+'.shtml','_blank')
+		 				// })
+		 				var $li_a = $('<a href="http://en.weather.com.cn/weather/'+fc_24_en.weatherinfo.cityid+'.shtml" target="_blank"></a>');
+		 				$li.append($li_a);
+		 				$li.children('h1').html(fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1));
+		 				$li.find("img").attr('src','http://i.tq121.com.cn/i/english/weaIcon/white/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png');
+		 				tool_pngfix();
+		 				$li.find("span").html(parseInt(fc_24_en.weatherinfo.temp1));
+		 				var indexVal = parseInt(fc_24_en.weatherinfo.indexval);
+		 				var indexVal = indexVal ? indexVal+'°':'';
+		 				$li.find('i').html(indexVal); 
+		 				if(cityHis_index < chNum.length){
+		 					cityHis_index++;   //递归
+		 					setWeather();
+		 				}
+						
+		 			}
+		 		})
+		 	}setWeather()
+		 	//北京 上海 广州 福州 重庆 西安 南宁 昆明 济南 武汉 三亚 哈尔滨
+		 	var weaConArr = [101010100,101020100,101280101,101230101,101040100,101110101,101300101,101290101,101120101,101200101,101310201,101050101];
+		 	var weaConArr_index = 0;
+		 	function weaConFun(){
+		 		var id = weaConArr[weaConArr_index];
+		 		$.ajax({
+		 			type:'GET',
+		 			url:'http://d1.weather.com.cn/fc_24_en/'+id+'.html',
+		 			dataType:'script',
+		 			cache:false,
+		 			async:false,
+		 			success:function(){
+		 				var $li = $("#ulStyle li").eq(weaConArr_index);
+		 				$li.children('span').html('<a target="_blank" href="http://en.weather.com.cn/weather/'+id+'.shtml">'+fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1)+'</a>');
+		 				$li.children('i').html(parseInt(fc_24_en.weatherinfo.temp1)+"°")
+		 				$li.children('img').attr('src','http://i.tq121.com.cn/i/english/weaIcon/blue/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png')			
+		 				tool_pngfix();
+		 				if(weaConArr_index<weaConArr.length){
+		 					weaConArr_index++;
+		 					weaConFun();
+		 				}
+		 			}
+		 		})
+		 	}weaConFun();
+		 }
+		 function weaTypeFunF(){
+		 	//local weather
+		 	var defaultCityHistory = '101010100,101020100,101280101';  //默认城市
+		 	cityHistory = $.cookie('cityHistory') || defaultCityHistory;  //get cookie
+		 	var chNum = cityHistory.split(',')
+		 	var cityHis_index = 0;	
+		 	$(".localWeather dt").html("°F")
+		 	function setWeather(){
+		 		$.ajax({
+		 			type:'GET',
+		 			url:"http://d1.weather.com.cn/fc_24_en/"+chNum[cityHis_index]+".html",
+		 			dataType:'script',
+		 			cache:false,
+		 			async:false,
+		 			success:function(){
+		 				var $li = $(".localWeather ul li").eq(cityHis_index);
+		 				$li.click(function(){
+		 					window.open('http://en.weather.com.cn/weather/'+fc_24_en.weatherinfo.cityid+'.shtml','_blank')
+		 				})
+		 				$li.children('h1').html(fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1));
+		 				$li.find("img").attr('src','http://i.tq121.com.cn/i/english/weaIcon/white/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png');
+		 				tool_pngfix();
+		 				$li.find("span").html(parseInt(fc_24_en.weatherinfo.tempF1));
+		 				var indexValf = parseInt(fc_24_en.weatherinfo.indexvalf);
+		 				var indexValf = indexValf?indexValf+'°':'';
+		 				$li.find('i').html(indexValf);
+		 				if(cityHis_index < chNum.length){
+		 					cityHis_index++;
+		 					setWeather()
+		 				}
+		 			}
+		 		})
+		 	}setWeather()
+		 	//北京 上海 广州 福州 重庆 西安 南宁 昆明 济南 武汉 三亚 哈尔滨
+		 	var weaConArr = [101010100,101020100,101280101,101230101,101040100,101110101,101300101,101290101,101120101,101200101,101310201,101050101];
+		 	var weaConArr_index = 0;
+		 	function weaConFun(){
+		 		var id = weaConArr[weaConArr_index];
+		 		$.ajax({
+		 			type:'GET',
+		 			url:'http://d1.weather.com.cn/fc_24_en/'+id+'.html',
+		 			dataType:'script',
+		 			cache:false,
+		 			async:false,
+		 			success:function(){
+		 				var $li = $("#ulStyle li").eq(weaConArr_index);
+		 				$li.children('span').html('<a target="_blank" href="http://en.weather.com.cn/weather/'+id+'.shtml">'+fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1)+'</a>');
+		 				$li.children('i').html(parseInt(fc_24_en.weatherinfo.tempF1)+"°")
+		 				$li.children('img').attr('src','http://i.tq121.com.cn/i/english/weaIcon/blue/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png')			
+		 				tool_pngfix();
+		 				if(weaConArr_index<weaConArr.length){
+		 					weaConArr_index++;
+		 					weaConFun();
+		 				}
+		 			}
+		 		})
+		 	}weaConFun();
+		 }
+		
 		//预警
 		$.ajax({
 			type:'GET',
@@ -47,143 +184,6 @@ define(function(require){
 			$(this).parent().animate({height:'80px'},400);
 			$(this).children('i').removeClass();
 		})
-
-
-		//温度格式切换
-		$("#weaUnit li:first").click(function(){
-			$(this).siblings().removeClass('on');
-			$(this).addClass('on');
-			weaTypeFunS();
-		}).click()
-		$("#weaUnit li:last").click(function(){
-			$(this).siblings().removeClass('on');
-			$(this).addClass('on');
-			weaTypeFunF();
-		})
-		
-		
-		//浏览历史 最近3个城市 Local Weather 模块 cookie
-		 function weaTypeFunS(){
-		 	//local weather
-		 	var defaultCityHistory = '101010100,101020100,101280101';  //默认城市 北上广
-		 	cityHistory = $.cookie('cityHistory') || defaultCityHistory;  //get cookie
-
-		 	var chNum = cityHistory.split(',');
-		 	var cityHis_index = 0;	
-		 	$(".localWeather dt").html("°C")
-		 	function setWeather(){  //递归法 解决for循环 异步执行 数据覆盖问题
-		 		$.ajax({
-		 			type:'GET',
-		 			url:"http://d1.weather.com.cn/fc_24_en/"+chNum[cityHis_index]+".html",
-		 			dataType:'script',
-		 			cache:false,
-		 			async:false,
-		 			success:function(type){
-		 				var $li = $(".localWeather ul li").eq(cityHis_index);
-		 				$li.click(function(){
-		 					window.open('http://en.weather.com.cn/weather/'+fc_24_en.weatherinfo.cityid+'.shtml','_blank')
-		 				})
-		 				//var $li_a = $('<a href="http://en.weather.com.cn/weather/'+fc_24_en.weatherinfo.cityid+'.shtml" target="_blank"></a>');
-		 				//$li.append($li_a);
-		 				$li.children('h1').html(fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1));
-		 				$li.find("img").attr('src','http://i.tq121.com.cn/i/english/weaIcon/white/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png');
-		 				tool_pngfix();
-		 				$li.find("span").html(parseInt(fc_24_en.weatherinfo.temp1));
-		 				var indexVal = parseInt(fc_24_en.weatherinfo.indexval);
-		 				var indexVal = indexVal ? indexVal+'°':'';
-		 				$li.find('i').html(indexVal); 
-		 				if(cityHis_index < chNum.length-1){
-		 					cityHis_index++;   //递归
-		 					setWeather();
-		 				}
-						
-		 			}
-		 		})
-		 	}setWeather()
-		 	//北京 上海 广州 福州 重庆 西安 南宁 昆明 济南 武汉 三亚 哈尔滨
-		 	var weaConArr = [101010100,101020100,101280101,101230101,101040100,101110101,101300101,101290101,101120101,101200101,101310201,101050101];
-		 	var weaConArr_index = 0;
-		 	function weaConFun(){
-		 		var id = weaConArr[weaConArr_index];
-		 		$.ajax({
-		 			type:'GET',
-		 			url:'http://d1.weather.com.cn/fc_24_en/'+id+'.html',
-		 			dataType:'script',
-		 			cache:false,
-		 			async:false,
-		 			success:function(){
-		 				var $li = $("#ulStyle li").eq(weaConArr_index);
-		 				$li.children('span').html('<a target="_blank" href="http://en.weather.com.cn/weather/'+id+'.shtml">'+fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1)+'</a>');
-		 				$li.children('i').html(parseInt(fc_24_en.weatherinfo.temp1)+"°")
-		 				$li.children('img').attr('src','http://i.tq121.com.cn/i/english/weaIcon/blue/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png')			
-		 				tool_pngfix();
-		 				if(weaConArr_index<weaConArr.length-1){
-		 					weaConArr_index++;
-		 					weaConFun();
-		 				}
-		 			}
-		 		})
-		 	}weaConFun();
-		 }
-		 function weaTypeFunF(){
-		 	//local weather
-		 	var defaultCityHistory = '101010100,101020100,101280101';  //默认城市
-		 	cityHistory = $.cookie('cityHistory') || defaultCityHistory;  //get cookie
-		 	var chNum = cityHistory.split(',')
-		 	var cityHis_index = 0;	
-		 	$(".localWeather dt").html("°F")
-		 	function setWeather(){
-		 		$.ajax({
-		 			type:'GET',
-		 			url:"http://d1.weather.com.cn/fc_24_en/"+chNum[cityHis_index]+".html",
-		 			dataType:'script',
-		 			cache:false,
-		 			async:false,
-		 			success:function(){
-		 				var $li = $(".localWeather ul li").eq(cityHis_index);
-		 				$li.click(function(){
-		 					window.open('http://en.weather.com.cn/weather/'+fc_24_en.weatherinfo.cityid+'.shtml','_blank')
-		 				})
-		 				$li.children('h1').html(fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1));
-		 				$li.find("img").attr('src','http://i.tq121.com.cn/i/english/weaIcon/white/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png');
-		 				tool_pngfix();
-		 				$li.find("span").html(parseInt(fc_24_en.weatherinfo.tempF1));
-		 				var indexValf = parseInt(fc_24_en.weatherinfo.indexvalf);
-		 				var indexValf = indexValf?indexValf+'°':'';
-		 				$li.find('i').html(indexValf);
-		 				if(cityHis_index < chNum.length-1){
-		 					cityHis_index++;
-		 					setWeather()
-		 				}
-		 			}
-		 		})
-		 	}setWeather()
-		 	//北京 上海 广州 福州 重庆 西安 南宁 昆明 济南 武汉 三亚 哈尔滨
-		 	var weaConArr = [101010100,101020100,101280101,101230101,101040100,101110101,101300101,101290101,101120101,101200101,101310201,101050101];
-		 	var weaConArr_index = 0;
-		 	function weaConFun(){
-		 		var id = weaConArr[weaConArr_index];
-		 		$.ajax({
-		 			type:'GET',
-		 			url:'http://d1.weather.com.cn/fc_24_en/'+id+'.html',
-		 			dataType:'script',
-		 			cache:false,
-		 			async:false,
-		 			success:function(){
-		 				var $li = $("#ulStyle li").eq(weaConArr_index);
-		 				$li.children('span').html('<a target="_blank" href="http://en.weather.com.cn/weather/'+id+'.shtml">'+fc_24_en.weatherinfo.city.substring(0,1).toUpperCase()+fc_24_en.weatherinfo.city.substring(1)+'</a>');
-		 				$li.children('i').html(parseInt(fc_24_en.weatherinfo.tempF1)+"°")
-		 				$li.children('img').attr('src','http://i.tq121.com.cn/i/english/weaIcon/blue/'+fc_24_en.weatherinfo.img1.substring(0,fc_24_en.weatherinfo.img1.indexOf('.gif'))+'.png')			
-		 				tool_pngfix();
-		 				if(weaConArr_index<weaConArr.length-1){
-		 					weaConArr_index++;
-		 					weaConFun();
-		 				}
-		 			}
-		 		})
-		 	}weaConFun();
-		 }
-		
 
 		 //"China Weather Conditions"变来变去的颜色样式,用jq来添加样式，确保html代码的一致性
 		 var $uSL = $("#ulStyle li");
