71c1b8c3bac950584ca2bb94783dd721790cfb43
liqiang <liqiang@weather.com.cn>
2014-04-10 09:13:02 +0800

    liqiang

--- source/j/english/toursm.js
+++ source/j/english/toursm.js
@@ -332,6 +332,18 @@ define(function(require) {
 		endRange: 1
 	});
 
+	var $T=$("#localTime");
+			function time(){
+			  var date=new Date();
+			  var weatherDate=date_0_9(date.getHours())+":"+date_0_9(date.getMinutes())+"&nbsp; "+date.getFullYear()+"-"+date_0_9(date.getMonth()+1)+"-"+date_0_9(date.getDate());
+			  $T.html("Local Time in China &nbsp; "+weatherDate);
+			}
+			time();
+			setInterval(time,60000);
 
+			function date_0_9(n){
+		  if(n<10) return '0'+n;
+			  else return n; 
+		}
 
 })
