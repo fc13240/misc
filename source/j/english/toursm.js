523f395e7f7d9bb6c68488627d86d7779c559c62
liqiang <liqiang@weather.com.cn>
2014-04-11 10:10:27 +0800

    liqiang

--- source/j/english/toursm.js
+++ source/j/english/toursm.js
@@ -100,21 +100,20 @@ define(function(require) {
 
 
 
-	var $T=$("#localTime");
-			function time(){
-			  var date=new Date();
-			  var weatherDate=date_0_9(date.getHours())+":"+date_0_9(date.getMinutes())+"&nbsp; "+date.getFullYear()+"-"+date_0_9(date.getMonth()+1)+"-"+date_0_9(date.getDate());
-			  $T.html("Local Time in China &nbsp; "+weatherDate);
-			}
-			time();
-			setInterval(time,60000);
-
-
 })
 
-		function date_0_9(n){
-		  if(n<10) return '0'+n;
-			  else return n; 
+		var $T=$("#localTime");
+		function time(){
+		  var date=new Date();
+		  var weatherDate=date_0_9(date.getHours())+":"+date_0_9(date.getMinutes())+"&nbsp; "+date.getFullYear()+"-"+date_0_9(date.getMonth()+1)+"-"+date_0_9(date.getDate());
+		  $T.html("Local Time in China &nbsp; "+weatherDate);
 		}
+		time();
+		setInterval(time,60000);
+
+		function date_0_9(n){
+	  if(n<10) return '0'+n;
+		  else return n; 
+	}
 
 })
