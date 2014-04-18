7f4aae22edb4b1573af0ca770dd447cc06443a0f
yuhan <yuh@weather.com.cn>
2014-04-18 15:53:32 +0800

    yuhan

--- shell/release-web.sh
+++ shell/release-web.sh
@@ -5,39 +5,45 @@ CURRENT_DIR=`dirname $0`
 WORK_DIR=`cd $CURRENT_DIR;cd ..;pwd`
 
 # rsync
-echo "<b>1. rsync min to online server1 61.4.185.223</b>";
+echo "<b>==============1. Rsync MIN to online server==============</b><br/>";
+
+echo "<b>(1) Server 1 61.4.185.223</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.223:/home/sam/www/misc/min/
-echo '==============';
+echo '===============================================================';
 echo '</pre>';
 
-echo "<b>2. rsync min to online server2 61.4.184.131</b>";
+echo "<b>(2) Server 2 61.4.184.131</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ wcc@61.4.184.131:/home/wcc/www/misc/min/
-echo '==============';
+echo '================================================================';
 echo '</pre>';
-
-echo "<b>3. rsync min to backup server1 61.4.185.212</b>";
-echo '<pre>';
-rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.212:/home/sam/www/misc/min/
-echo '==============';
-echo '</pre>';
-
-echo "<b>4. rsync source to online server1 61.4.185.223</b>";
+echo '<br/>';
+echo "<b>==============2. Rsync SOURCE to online server=============</b><br/>";
+echo "<b>(1) Server 1 61.4.185.223</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.223:/home/sam/www/misc/source/
-echo '==============';
+echo '===============================================================';
 echo '</pre>';
 
-echo "<b>5. rsync source to online server2 61.4.184.131</b>";
+echo "<b>(2) Server2 61.4.184.131</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ wcc@61.4.184.131:/home/wcc/www/misc/source/
-echo '==============';
+echo '===============================================================';
 echo '</pre>';
 
-echo "<b>6. rsync source to backup server1 61.4.185.212</b>";
+echo '<br/>';
+echo "<b>==============3. Rsync to backup server=============</b><br/>";
+
+echo "<b>(1) Rsync min to 61.4.185.212</b>";
+echo '<pre>';
+rsync -vaz '-e ssh -p 2222'  $WORK_DIR/min/ sam@61.4.185.212:/home/sam/www/misc/min/
+echo '===============================================================';
+echo '</pre>';
+
+echo "<b>(2) Rsync source to 61.4.185.212</b>";
 echo '<pre>';
 rsync -vaz '-e ssh -p 2222'  $WORK_DIR/source/ sam@61.4.185.212:/home/sam/www/misc/source/
-echo '==============';
+echo '===============================================================';
 echo '</pre>';
 echo '<br/>';
