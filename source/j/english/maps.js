f7d1daffd1b007e04556e04961b9dadc1db1d36f
liujinyu <13581642285@163.com>
2014-04-25 17:02:32 +0800

    ljy

--- source/j/english/maps.js
+++ source/j/english/maps.js
@@ -6,57 +6,57 @@ define(function(require){
 		var index = parseInt(document.URL.substring(document.URL.indexOf('index=')+6))||1;
 		var index = index>19||index<1?1:index;
 		var $tA = $('.imgNav .t a');
-		
 
 		//隐藏温度单位转换按钮
 		$('#weaUnit').hide();
 
-
 		switch(index){
-			case 1:_a_on(index);getData('YB_WD_ZG24');rightData = [{dataId:'YB_WD_ZD24',ind:'?index=6'},{dataId:'JC_WD_ZG10',ind:'?index=7'},{dataId:'JC_WD_ZG20',ind:'?index=8'}]; break;//24小时最高温度预报
-			case 2:_a_on(index);getData('YB_JSL_024');rightData = [{dataId:'YB_JSL_048',ind:'?index=9'},{dataId:'JC_JSL_02405',ind:'?index=10'},{dataId:'JC_JSL_10',ind:'?index=11'}]; break;//全国24小时降水量预报
-			case 3:_a_on(index);getRoad();rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;
+			case 1:_a_on(index);getData('YB_WD_ZG24');var rightData = [{dataId:'YB_WD_ZD24',ind:'?index=6'},{dataId:'JC_WD_ZG10',ind:'?index=7'},{dataId:'JC_WD_ZG20',ind:'?index=8'}]; break;//24小时最高温度预报
+			case 2:_a_on(index);getData('YB_JSL_024');var rightData = [{dataId:'YB_JSL_048',ind:'?index=9'},{dataId:'JC_JSL_02405',ind:'?index=10'},{dataId:'JC_JSL_10',ind:'?index=11'}]; break;//全国24小时降水量预报
+			case 3:_a_on(index);getRoad();var rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;
 			//全国主要公路气象预报
 			case 4:_a_on(index);typFlah();break;//台风flash动画
-			case 5:_a_on(index);getData('YB_KQWR_24');rightData = [{dataId:'YB_M_24',ind:'?index=18'},{dataId:'YB_W_24',ind:'?index=19'}];break;//空气污染气象条件预报
-			case 6:_a_on(1);getData('YB_WD_ZD24');rightData = [{dataId:'YB_WD_ZG24',ind:'?index=1'},{dataId:'JC_WD_ZG10',ind:'?index=7'},{dataId:'JC_WD_ZG20',ind:'?index=8'}]; break;//24小时最低温度预报
-			case 7:_a_on(1);getData('JC_WD_ZG10');rightData = [{dataId:'YB_WD_ZD24',ind:'?index=6'},{dataId:'YB_WD_ZG24',ind:'?index=1'},{dataId:'JC_WD_ZG20',ind:'?index=8'}]; break;//全国10天最高气温分布
-			case 8:_a_on(1);getData('JC_WD_ZG20');rightData = [{dataId:'YB_WD_ZD24',ind:'?index=6'},{dataId:'JC_WD_ZG10',ind:'?index=7'},{dataId:'YB_WD_ZG24',ind:'?index=1'}]; break;//全国20天最高气温分布
+			case 5:_a_on(index);getData('YB_KQWR_24');var rightData = [{dataId:'YB_M_24',ind:'?index=18'},{dataId:'YB_W_24',ind:'?index=19'}];break;//空气污染气象条件预报
+			case 6:_a_on(1);getData('YB_WD_ZD24');var rightData = [{dataId:'YB_WD_ZG24',ind:'?index=1'},{dataId:'JC_WD_ZG10',ind:'?index=7'},{dataId:'JC_WD_ZG20',ind:'?index=8'}]; break;//24小时最低温度预报
+			case 7:_a_on(1);getData('JC_WD_ZG10');var rightData = [{dataId:'YB_WD_ZD24',ind:'?index=6'},{dataId:'YB_WD_ZG24',ind:'?index=1'},{dataId:'JC_WD_ZG20',ind:'?index=8'}]; break;//全国10天最高气温分布
+			case 8:_a_on(1);getData('JC_WD_ZG20');var rightData = [{dataId:'YB_WD_ZD24',ind:'?index=6'},{dataId:'JC_WD_ZG10',ind:'?index=7'},{dataId:'YB_WD_ZG24',ind:'?index=1'}]; break;//全国20天最高气温分布
 
-			case 9:_a_on(2);getData('YB_JSL_048');rightData = [{dataId:'YB_JSL_024',ind:'?index=2'},{dataId:'JC_JSL_02405',ind:'?index=10'},{dataId:'JC_JSL_10',ind:'?index=11'}]; break;//全国48小时降水量预报
-			case 10:_a_on(2);getData('JC_JSL_02405');rightData = [{dataId:'YB_JSL_048',ind:'?index=9'},{dataId:'YB_JSL_024',ind:'?index=2'},{dataId:'JC_JSL_10',ind:'?index=11'}]; break;//全国降水量实况
-			case 11:_a_on(2);getData('JC_JSL_10');rightData = [{dataId:'YB_JSL_048',ind:'?index=9'},{dataId:'JC_JSL_02405',ind:'?index=10'},{dataId:'YB_JSL_024',ind:'?index=2'}]; break;//全国近10天降水量实况
+			case 9:_a_on(2);getData('YB_JSL_048');var rightData = [{dataId:'YB_JSL_024',ind:'?index=2'},{dataId:'JC_JSL_02405',ind:'?index=10'},{dataId:'JC_JSL_10',ind:'?index=11'}]; break;//全国48小时降水量预报
+			case 10:_a_on(2);getData('JC_JSL_02405');var rightData = [{dataId:'YB_JSL_048',ind:'?index=9'},{dataId:'YB_JSL_024',ind:'?index=2'},{dataId:'JC_JSL_10',ind:'?index=11'}]; break;//全国降水量实况
+			case 11:_a_on(2);getData('JC_JSL_10');var rightData = [{dataId:'YB_JSL_048',ind:'?index=9'},{dataId:'JC_JSL_02405',ind:'?index=10'},{dataId:'YB_JSL_024',ind:'?index=2'}]; break;//全国近10天降水量实况
 		
-			case 14:_a_on(3);getData('YB_GL_SJL_24H');rightData = [{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;//公路24小时降水量预报
-			case 15:_a_on(3);getData('YB_TL_SJL_24H');rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;//铁路24小时降水量预报
-			case 16:_a_on(3);getData('JC_GL_NJD_ZH');rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;//公路逐小时能见度实况
-			case 17:_a_on(3);getData('JC_TL_NJD_ZH');rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'}];break;//铁路逐小时能见度实况
+			case 14:_a_on(3);getData('YB_GL_SJL_24H');var rightData = [{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;//公路24小时降水量预报
+			case 15:_a_on(3);getData('YB_TL_SJL_24H');var rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;//铁路24小时降水量预报
+			case 16:_a_on(3);getData('JC_GL_NJD_ZH');var rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_TL_NJD_ZH',ind:'?index=17'}];break;//公路逐小时能见度实况
+			case 17:_a_on(3);getData('JC_TL_NJD_ZH');var rightData = [{dataId:'YB_GL_SJL_24H',ind:'?index=14'},{dataId:'YB_TL_SJL_24H',ind:'?index=15'},{dataId:'JC_GL_NJD_ZH',ind:'?index=16'}];break;//铁路逐小时能见度实况
 
-			case 18:_a_on(5);getData('YB_M_24');rightData = [{dataId:'YB_KQWR_24',ind:'?index=5'},{dataId:'YB_W_24',ind:'?index=19'}]; break;//24小时霾预报
-			case 19:_a_on(5);getData('YB_W_24');rightData = [{dataId:'YB_M_24',ind:'?index=18'},{dataId:'YB_KQWR_24',ind:'?index=5'}]; break;//24小时大雾预报
+			case 18:_a_on(5);getData('YB_M_24');var rightData = [{dataId:'YB_KQWR_24',ind:'?index=5'},{dataId:'YB_W_24',ind:'?index=19'}]; break;//24小时霾预报
+			case 19:_a_on(5);getData('YB_W_24');var rightData = [{dataId:'YB_M_24',ind:'?index=18'},{dataId:'YB_KQWR_24',ind:'?index=5'}]; break;//24小时大雾预报
 		}
 		
 		//右侧相关产品的数据获取
 		var setRigIndex = 0;
 		function setRight(){
-			$.ajax({
-				type:'GET',
-				url:'http://d1.weather.com.cn/product_json/'+rightData[setRigIndex].dataId+'.html',
-				dataType:'script',
-				async:false,
-				success:function(){
-					var arrTime = jsl.jsinfo[0].dt.split(' ');
-					var strDiv = '<div class="mt clearfix"><a href="'+rightData[setRigIndex].ind+'"><img width="180" height="125" class="fl" src="http://i.weather.com.cn/i/product/pic/s/'+jsl.jsinfo[0].fn+'"></a><div class="fl r"><p class="txt">'+jsl.jsinfo[0].descpri+'</p><div class="time"><p>'+arrTime[1]+arrTime[2].toLowerCase()+'</p><p>'+arrTime[0]+'</p>';
-					$('.map').append(strDiv);
-					if (setRigIndex==rightData.length-1) {
-						$('.map').height($('.map div.mt').length*135-5);
-					};
-					if (setRigIndex<rightData.length-1) {
-						setRigIndex++;
-						setRight();
-					};
-				}
-			})
+			if (typeof rightData !== 'undefined'){
+				$.ajax({
+					type:'GET',
+					url:'http://d1.weather.com.cn/product_json/'+rightData[setRigIndex].dataId+'.html',
+					dataType:'script',
+					async:false,
+					success:function(){
+						var arrTime = jsl.jsinfo[0].dt.split(' ');
+						var strDiv = '<div class="mt clearfix"><a href="'+rightData[setRigIndex].ind+'"><img width="180" height="125" class="fl" src="http://i.weather.com.cn/i/product/pic/s/'+jsl.jsinfo[0].fn+'"></a><div class="fl r"><a class="txt" href="'+rightData[setRigIndex].ind+'">'+jsl.jsinfo[0].descpri+'</a><div class="time"><p>'+arrTime[1]+arrTime[2].toLowerCase()+'</p><p>'+arrTime[0]+'</p>';
+						$('.map').append(strDiv);
+						if (setRigIndex==rightData.length-1) {
+							$('.ad1').show()
+						};
+						if (setRigIndex<rightData.length-1) {
+							setRigIndex++;
+							setRight();
+						};
+					}
+				})
+			}
 		}setRight(); 
 
 		//主体内容的图片、说明的信息获取
@@ -94,7 +94,8 @@ define(function(require){
 			var iframe = '<iframe width="630" scrolling="no" height="488" frameborder="0" src="http://flash.weather.com.cn/typhoon/taifeng.swf" name="surf"></iframe>'
 			$('.show').empty().append(iframe);
 			var strDiv = '<div class="mt clearfix"><a href="http://en.weather.com.cn/satellite/"><img width="180" height="125" class="fl" src="http://i.tq121.com.cn/i/english/cityInfo/map2.jpg"></a><div class="d1"><div class="d2"><div class="d3"><a href="http://en.weather.com.cn/satellite/">China Satellite Image</a></div></div></div></div><div class="mt clearfix"><a href="http://en.weather.com.cn/radar/"><img width="180" height="125" class="fl" src="http://i.tq121.com.cn/i/english/cityInfo/map1.jpg"></a><div class="d1"><div class="d2"><div class="d3"><a href="http://en.weather.com.cn/radar/">China Radar</a></div></div></div></div>';
-			$('.map').append(strDiv).height(262);
+			$('.map').append(strDiv);
+			$('.ad1').show();
 
 		}
 		//二级导航的选中样式
