131ff62ce4d4eac3ac73d09abf9f3e7b4ef3e354
liujinyu <13581642285@163.com>
2014-05-28 10:30:29 +0800

    ljy

--- source/j/index/popup.js
+++ source/j/index/popup.js
@@ -1,4 +1,75 @@
 // JavaScript Document
+var arrInter = [];
+var $Ul = $('.localPopup ul');
+$('.localPopup .r a').live('click',function(){	
+	var $li = $(this).parents('li');	
+	$li.remove();
+	var file = $.cookie('closeWarn') || '';
+	var file = file +','+ $li.find('span').children('a').attr('href').split('=')[1];
+	$.cookie('closeWarn',file,{expires:30,path: '/'})
+})
+//预警
+var kindObj = {'01':'台风','02':'暴雨','03':'暴雪','04':'寒潮','05':'大风','06':'沙尘暴','07':'高温','08':'干旱','09':'雷电','10':'冰雹','11':'霜冻','12':'大雾','13':'霾','14':'道路结冰',
+'91':'寒冷','92':'灰霾','93':'雷雨大风','94':'森林火险','95':'降温','96':'道路冰雪','97':'干热风','98':'低温','99':'冰冻'};
+var gradeObj={'01':'蓝色','02':'黄色','03':'橙色','04':'红色','91':'白色'};
+var $ifurl = "http://product.weather.com.cn/alarm/grepalarm_cn.php";
+function getInfo(){
+    $.ajax({
+        "type": "GET",
+        "url": $ifurl,
+        "dataType": "script",
+        "cache":false,      
+        "success": function() {
+            if (typeof id != 'undefined') {
+                var localCityId = id.toString().substr(0,7);
+				var localProId =  localCityId.substr(0,5);
+				var strCookie = $.cookie('closeWarn') || '';
+				var arrCookie = strCookie.split(",");
+				var li='' 
+				for (var i = alarminfo.data.length - 1; i >= 0; i--) {
+					var arrId = alarminfo.data[i][1].split('-');
+					if((arrId[0].length == 5 && arrId[0] == localProId)||(arrId[0].length == 7 && arrId[0] == localCityId)||(arrId[0] == id)){
+						var al = true;
+						for (var j = arrCookie.length - 1; j >= 0; j--) {
+							if(alarminfo.data[i][1] == arrCookie[j]){
+								var al = false;
+							}
+						};
+						if (al) {
+							var kind = arrId[2].substr(0,2);
+							var grade = arrId[2].substr(2,2);				
+							li += '<li><i class="l" style="background-image:url(&quot;http://10.14.85.90/source/i/Popup/warn_'+grade+'.png&quot;);"></i><span style="background-image:url(&quot;http://10.14.85.90/source/i/Popup/warn_'+grade+'.png&quot;);"><a target="_blank" href="http://www.weather.com.cn/alarm/newalarmcontent.shtml?file='+alarminfo.data[i][1]+'">'+ alarminfo.data[i][0] +'气象台发布'+ kindObj[kind] + gradeObj[grade] +'预警</a></span><i style="background-image:url(&quot;http://10.14.85.90/source/i/Popup/warn_'+grade+'.png&quot;);" class="r"><a></a></i></li>';	
+						};
+					}
+				};
+				$Ul.html(li);
+            }
+        }
+    })
+}getInfo();
+arrInter.push(setInterval(getInfo, 1000*60));
+//滚动
+var grollIndex=0;
+function groll(){
+	var $Ul = $(".localPopup ul");
+	var n = $Ul.children('li').length;
+	grollIndex++;
+	if(grollIndex>=n) grollIndex=0;
+	$Ul.animate({marginTop:-55*grollIndex+'px'},'fast');
+}
+$('.localPopup').hover(function(){
+	clearInter(arrInter);
+},function(){
+	arrInter.push(setInterval(groll, 1000*3));
+})
+arrInter.push(setInterval(groll, 1000*3));
+//clearInter
+function clearInter(arrInter){
+	for (var i = arrInter.length - 1; i >= 0; i--) {
+		clearInterval(arrInter[i]);
+	}
+}	
+//cookie
 jQuery.cookie = function(name, value, options) {
 	if (typeof value != 'undefined') { // name and value given, set cookie
 		options = options || {};
@@ -37,52 +108,3 @@ jQuery.cookie = function(name, value, options) {
 		return cookieValue;
 	}
 };
-
-//预警
-var $yjlb = ["台风", "暴雨", "暴雪", "寒潮", "大风", "沙尘暴", "高温", "干旱", "雷电", "冰雹", "霜冻", "大雾", "霾", "道路结冰"];
-var $gdlb = ["寒冷", "灰霾", "雷雨大风", "森林火险", "降温", "道路冰雪", "干热风", "低温", "冰冻"];
-var $yjyc = ["蓝色", "黄色", "橙色", "红色"];
-var $gdyc = ["白色"];
-var $ifurl = "http://product.weather.com.cn/alarm/newIndexalarm.php";
-$('.localPopup .r a').click(function(){	
-	$('.localPopup').hide();
-	var file = $('.localPopup span a').attr('href').split('=')[1];
-	$.cookie('closeWarn',file,{expires:30,path: '/'})
-})
-function getInfo(){
-    $.ajax({
-        "type": "GET",
-        "url": $ifurl,
-        "dataType": "script",        
-        "success": function() {
-            if (alarminfo.pr.length > 0) {
-                var localId = id.toString().substr(0,5);
-				var closeWarn = $.cookie('closeWarn');
-    			$.each(alarminfo.pr,function(i,k){
-					if(k[1] != closeWarn){
-						var $filename = k[1];
-						var cityId = $filename.split('-')[0];
-						if(cityId == localId){ 
-							$pos = $filename.lastIndexOf("-"),
-							$lb = $filename.substr($pos + 1, 2),
-							$jb = $filename.substr($pos + 3, 2),
-							$textlb = $yjlb[parseInt($lb, 10) - 1],
-							$textyc = $yjyc[parseInt($jb, 10) - 1];
-							$lb > 90 && ($textlb = $gdlb[parseInt($lb, 10) - 91]),
-							$jb > 90 && ($textyc = $gdyc[parseInt($jb, 10) - 91]),
-							$(".localPopup i a").hover(function(){
-								$(this).css('backgroundImage','url(http://10.14.85.90/source/i/Popup/warn_'+ $jb +'.png)');
-							},function(){
-								$(this).css('backgroundImage','none');
-							});
-							$('.localPopup i,.localPopup span').css('backgroundImage','url(http://10.14.85.90/source/i/Popup/warn_'+ $jb +'.png)');
-							$('.localPopup span').html('<a target="_blank" href="http://www.weather.com.cn/alarm/newalarmcontent.shtml?file=' + $filename + '">' + k[0] + '气象台发布' + $textlb + $textyc + '预警</a>');
-							$('.localPopup').show();
-						};
-					}
-    			})
-            }
-        }
-    })
-}getInfo()
-setInterval(getInfo, 1000*60)
