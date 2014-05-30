f3986f6edfa610cd68d0ef6f4c9e91bd99ec9dc2
liujinyu <13581642285@163.com>
2014-05-30 16:13:31 +0800

    ljy

--- source/j/mobile/citmani.js
+++ source/j/mobile/citmani.js
@@ -1,7 +1,7 @@
 $(function(){
 	var $firLi = $('.cityman ul li:first');
 	var localCity = $.cookie('localCity') || "";
-	var localHref = localCity && 'http://wap.weather.com.cn/tq/'+localCity+'.shtml' || ''
+	var localHref = localCity && 'http://wap.weather.com.cn/'+localCity+'.shtml' || ''
 
 	$firLi.children('a').attr('href',localHref);
 
@@ -10,14 +10,14 @@ $(function(){
 	var $firLi = $('.cityman ul li:first');
 	for (var i = arrCity.length - 1; i > 0; i--) {
 		var arr = arrCity[i].split(',');
-		$firLi.after('<li><a href="http://wap.weather.com.cn/tq/'+parseInt(arr[0])+'.shtml">'+arr[1]+'</a><img src="http://i.tq121.com.cn/i/mobile/images/cha.png"></li>') 
+		$firLi.after('<li><a href="http://wap.weather.com.cn/'+arr[0]+'.shtml">'+arr[1]+'</a><img src="http://i.tq121.com.cn/i/mobile/images/cha.png"></li>') 
 	};
 
 	$('.cityman ul li img').live('click',function(){
 		$(this).parents('li').remove();
 		
 		for (var i = arrCity.length - 1; i > 0; i--) {
-			arrCity[i].split(',')[0] == $(this).prev().attr('href').match(/\d{5,}/) && arrCity.splice(i,1);
+			arrCity[i].split(',')[0] == $(this).prev().attr('href').match(/\d{5,}\w*/) && arrCity.splice(i,1);
 		};
 		citysL = arrCity.join('/');
 		$.cookie('citys',citysL,{expires:30,path: '/'});
