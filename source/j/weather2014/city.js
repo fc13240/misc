a091ecd263cac7772ffd2ccf0b817fd5c7c055a5
liujinyu <13581642285@163.com>
2014-06-19 20:07:07 +0800

    ljy

--- source/j/weather2014/city.js
+++ source/j/weather2014/city.js
@@ -27,15 +27,28 @@ define(function(require){
         }).roll();
 		var botUlWidth = $('#scrollPic img').length*25;
 		$('#scrollPic ul:not(:has(img)):last').css('marginLeft',150-botUlWidth/2+'px').width(botUlWidth	);
-		//今天 明后天导航hover click效果
-		var dd = $('input#whichDay[type=hidden]').attr('value');
+		//空气质量级别背景色
+		var $airSpan = $('.m li.sk .air span');
+		var levelNum = parseInt($airSpan.text());
+		var levelColor = levelNum<50 && "#44cf12" || levelNum<100 && '#f2e75a' || levelNum<150 && "#f6b42c" && levelNum<200 && "#fa5535" || levelNum<300 && "#e31b40" || "#8e0636";
+		$airSpan.css('backgroundColor',levelColor);
+
+		//页面默认执行
+		var dd = $('input#whichDay[type=hidden]').attr('value');		
 		$('#someDayNav li[data-day='+dd+']').addClass('on').siblings()._hover();
-		
-		//白天和黑夜之间的切换
-		var $todayUl = $('.left .m ul.b');
-		$todayUl.children('li[data-dn=n]').hide();
-		$('#someDayLi li.dn').click(function(){
+		$('#'+dd).siblings(".m").remove();
+		var dataDn = $('#'+dd).find('.on').attr('data-dn');
+		var $todayUl = $('#Hour3');
+		$todayUl.find('li[data-dn='+dataDn+']').show();
+		//今天 明后天 7d 15d 联动
+		var $todayUl = $('#Hour3');
+		$('#tabDays li.dn').click(function(){
 			$(this).addClass('on').siblings().removeClass('on');
+			var $img = $(this).find('img');
+			var imgName = $img.attr('src').match(/\w\d+\.jpg/g);
+			$img.attr('src','http://10.14.85.90/source/i/weather2014/jpg/blue/'+imgName);
+			$(this).siblings(".dn").find('img').attr('src','http://10.14.85.90/source/i/weather2014/jpg/gray/'+imgName)
+			// "http://10.14.85.90/source/i/weather2014/jpg/blue/d01.jpg"
 			var dn = $(this).attr('data-dn');
 			$todayUl.children('li[data-dn='+dn+']').show();
 			$todayUl.children('li[data-dn!='+dn+']').hide();
